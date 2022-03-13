44823f83ef3636a0e6937d181f346b5334b2e1bd
lizheming <i@imnerd.org>
2022-03-13 15:13:37 +0800

    fix: fix commonjs export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import storages, { IStorages } from './storage';
-
 function Dittorm<K extends keyof typeof storages>(type: K) {
   if (!type) {
     throw Error('type is required!');
@@ -15,4 +14,7 @@ function Dittorm<K extends keyof typeof storages>(type: K) {
   }
 }
 
-export default Dittorm;
+// https://github.com/microsoft/TypeScript/issues/2719
+// https://remarkablemark.org/blog/2020/05/05/typescript-export-commonjs-es6-modules/
+Dittorm.default = Dittorm;
+export = Dittorm;
